1a67694d472e3189089f95cfd9f03ff04f19af81
Aleksejs <aleks4444@inbox.lv>
2020-07-14 15:39:50 +0300

    add api

--- src/Entity/Project.php
+++ src/Entity/Project.php
@@ -2,12 +2,18 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\ProjectRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
+ * @ApiResource(
+ *     collectionOperations={"get"={"normalization_context"={"groups":"project:list"}}},
+ *     itemOperations={"get"={"normalization_context"={"groups":"project:list"}}}
+ * )
  * @ORM\Entity(repositoryClass=ProjectRepository::class)
  */
 class Project
@@ -16,16 +22,19 @@ class Project
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
+     * @Groups({"project:list"})
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"project:list"})
      */
     private $title;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"project:list"})
      */
     private $authorsName;
 
