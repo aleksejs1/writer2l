4722b34460cfd21279cebeee0bbb290b12ac15b3
Aleksejs <aleks4444@inbox.lv>
2020-07-17 15:45:05 +0300

    implement WYSIWYG editor

--- assets/js/app.js
+++ assets/js/app.js
@@ -53,6 +53,37 @@ function updateSceneNote() {
     );
 }
 
+function initEditor() {
+    if ($("#scene_content").length) {
+        $("#scene_content").hide();
+        $("<div id='custom-editor' contentEditable  class='form-control custom-editor'></div>").insertAfter("#scene_content");
+        $("<div id='custom-editor-underline' class='btn btn-light'><u>U</u></div>").insertAfter("#scene_content");
+        $("<div id='custom-editor-italic' class='btn btn-light'><i>I</i></div>").insertAfter("#scene_content");
+        $("<div id='custom-editor-bold' class='btn btn-light'><b>B</b></div>").insertAfter("#scene_content");
+        $("#custom-editor").html($("#scene_content").val());
+        document.execCommand("defaultParagraphSeparator", false, "p");
+        $("#custom-editor").on('keyup', function (e) {
+            $("#scene_content").val($("#custom-editor").html());
+        });
+        $('#custom-editor-bold').on('mousedown', function (e) {
+            document.execCommand('bold', false, '');
+            window.getSelection().getRangeAt(0).startContainer;
+            e.preventDefault();
+        });
+        $('#custom-editor-italic').on('mousedown', function (e) {
+            document.execCommand('italic', false, '');
+            window.getSelection().getRangeAt(0).startContainer;
+            e.preventDefault();
+        });
+        $('#custom-editor-underline').on('mousedown', function (e) {
+            document.execCommand('underline', false, '');
+            window.getSelection().getRangeAt(0).startContainer;
+            e.preventDefault();
+        });
+    }
+}
+
 $(".toggle-sort").on('click', toggleSortButtons);
 $("#scene_description_ajax").on('change', updateSceneDescription);
 $("#scene_note_ajax").on('change', updateSceneNote);
+initEditor();
